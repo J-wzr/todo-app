444d98e109e789c9554a21a23bba0fc3248ac38c
J-wzr <joshuamwesigwa58@gmail.com>
2023-10-29 14:44:53 +0300

    The user can add, and delete the todos

--- functions.py
+++ functions.py
@@ -1,5 +1,9 @@
 
 def get_todos():
-    with open("todos.txt") as file:
+    with open("todos.txt", "r") as file:
         todos = file.readlines()
     return todos
+
+def add_todos(todo_arg):
+    with open("todos.txt", "w") as file:
+        file.writelines(todo_arg)
--- home.py
+++ home.py
@@ -1,10 +1,29 @@
 import streamlit as st
 import functions
 
+
+todos  = functions.get_todos()
+def add_todo():
+    todo = st.session_state['new_todo']
+    todo = todo.strip().capitalize() + "\n"
+    todos.append(todo)
+
+    functions.add_todos(todos)
+
+
 st.title("My Todo app")
 st.subheader("This app will improve your productivity in your endeavors")
 
 
-todos  = functions.get_todos()
-for item in todos:
-    st.checkbox(item)
+
+for index, item in enumerate(todos):
+    checkbox = st.checkbox(item, key=item)
+    if checkbox:
+        todos.pop(index)
+        functions.add_todos(todos)
+        del st.session_state[item]
+        st.experimental_rerun()
+    
+
+st.text_input(label="Add todos", placeholder="Enter todo...",
+              on_change=add_todo, key="new_todo")
